3ccf0d7bf982411f19e4fa7975bd53f9aa8e861c
D3v1f <108231428+D3v1f@users.noreply.github.com>
2025-01-06 22:35:41 -0500

    Add files via upload

---
+++ Programacion1/EjemplosWhile/ejemplos2.java
@@ -0,0 +1,24 @@
+package code.while_ejemplo;
+
+import java.util.Scanner;
+public class ejemplos2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int i;
+        String palabra, inversa="";
+        // ejemplo de decremento
+        // escribir una palabra inversa
+        System.out.print("Ingrese una palabra: ");
+        palabra = entrada.next();
+        
+        i = palabra.length();
+        while (i > 0) {
+            inversa = inversa+palabra.charAt(i-1);
+            i--;
+        }
+        entrada.close();
+        System.out.println("");
+        System.out.println("Palabra normal: " + palabra);
+        System.out.println("Palabra inversa: " + inversa);
+    }
+}
