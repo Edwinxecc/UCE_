0a84483185ffe873ecb5300c960a0821c29c9d0f
Edwin <caizadavid417@gmail.com>
2025-05-17 11:39:46 -0500

    add PAE_U2MiFecha

---
+++ ProgramacionII/PAE_U2.1Fecha/src/main/java/ec/edu/uce/Main.java
@@ -0,0 +1,7 @@
+package ec.edu.uce;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ProgramacionII/PAE_U2.1Fecha/src/main/java/ec/edu/uce/dominio/MiFecha.java
@@ -0,0 +1,20 @@
+package ec.edu.uce.dominio;
+
+public class MiFecha {
+    private int dia;
+    private int mes;
+    private int anio;
+
+    public MiFecha(){
+        this.dia = 1;
+        this.mes = 1;
+        this.anio = 1990;
+    }
+
+    public MiFecha (int dia, int mes, int anio){
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+
+}
