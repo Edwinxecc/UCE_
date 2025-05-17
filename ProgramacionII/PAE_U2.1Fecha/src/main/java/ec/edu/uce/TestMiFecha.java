3d08c4a79e3a3da80b56334761ea1047dc692960
Edwin <caizadavid417@gmail.com>
2025-05-17 14:44:10 -0500

    Ending

--- ProgramacionII/PAE_U2.1Fecha/src/main/java/ec/edu/uce/TestMiFecha.java
+++ ProgramacionII/PAE_U2.1Fecha/src/main/java/ec/edu/uce/TestMiFecha.java
@@ -12,44 +12,48 @@ public class TestMiFecha {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MiFecha fecha = new MiFecha(); // Por defecto: 01/01/1990
-        boolean continuar = true;
+        int opcion;
+        do {
+            System.out.println("\n\n\nSelecione una opcion: ");
+            System.out.println("[1] Contructor Por Defecto");
+            System.out.println("[2] Contructor con Parametros");
+            System.out.println("[3] Probar Getters");
+            System.out.println("[4] Probar Setters");
+            System.out.println("[5] Validar Fecha");
+            System.out.println("[0] Salir");
+             opcion = sc.nextInt();
 
-
-
-        int opcion = sc.nextInt();
-
-        switch (opcion) {
-            case 1:
-                fecha = probarConstructorPorDefecto();
-                break;
-            case 2:
-                fecha = probarConstructorConParametros(sc);
-                break;
-            case 3:
-                probarGetters(fecha);
-                break;
-            case 4:
-                probarSetters(fecha, sc);
-                break;
-            case 5:
-                probarFechaValida(fecha);
-                fecha = new MiFecha(30, 2, 2020);
-                probarFechaValida(fecha);
-                break;
-                /*case 0:
-                    continuar = false;
-                    System.out.println("Fin de pruebas.");
+            switch (opcion) {
+                case 1:
+                    fecha = probarConstructorPorDefecto();
+                    break;
+                case 2:
+                    fecha = probarConstructorConParametros(sc);
+                    break;
+                case 3:
+                    probarGetters(fecha);
+                    break;
+                case 4:
+                    probarSetters(fecha, sc);
+                    break;
+                case 5:
+                    probarFechaValida(fecha);
+                    fecha = new MiFecha(30, 2, 2020);
+                    probarFechaValida(fecha);
                     break;
                 default:
-                    System.out.println("Opción inválida. Intente de nuevo.");*/
-        }
-
+                    if (opcion == 0) {
+                        System.out.println("[*] Saliendo");
+                    }
+                    else {System.out.println("[*] Opcion incorrecta");}
+            }
+        }while (opcion != 0);
 
         sc.close();
     }
 
     public static MiFecha probarConstructorPorDefecto() {
-        //System.out.println("\n==> Constructor por defecto:");
+        System.out.println("\n== Constructor por defecto ==");
         MiFecha f = new MiFecha();
         System.out.println("Fecha: " + f.getDia() + "/" + f.getMes() + "/" + f.getAnio());
         return f;
@@ -63,14 +67,14 @@ public class TestMiFecha {
     }
 
     public static void probarGetters(MiFecha f) {
-        //System.out.println("\n==> Getters:");
+        System.out.println("\n== Getters ==");
         System.out.println("Día: " + f.getDia());
         System.out.println("Mes: " + f.getMes());
         System.out.println("Año: " + f.getAnio());
     }
 
     public static void probarSetters(MiFecha f, Scanner sc) {
-        //System.out.println("\n==> Setters:");
+        System.out.println("\n== Setters ==");
 
         System.out.println("Set día: " + f.setDia(20));
         System.out.println("Set mes: " + f.setMes(2));
@@ -79,7 +83,7 @@ public class TestMiFecha {
     }
 
     public static void probarFechaValida(MiFecha f) {
-        //System.out.println("\n==> Validación de fecha:");
+        System.out.println("\n== Validación de fecha ==");
         System.out.println("Fecha: " + f.getDia() + "/" + f.getMes() + "/" + f.getAnio());
         System.out.println("¿Es válida? " + f.fechaValida());
     }
