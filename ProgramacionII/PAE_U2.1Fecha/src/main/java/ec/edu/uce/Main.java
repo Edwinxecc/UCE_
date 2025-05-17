2f153034236d9b30b3c8720be8b86c6b3327ccb7
Edwin <caizadavid417@gmail.com>
2025-05-17 14:26:22 -0500

    add TestMiFecha

--- ProgramacionII/PAE_U2.1Fecha/src/main/java/ec/edu/uce/Main.java
+++
@@ -1,7 +0,0 @@
-package ec.edu.uce;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- ProgramacionII/PAE_U2.1Fecha/src/main/java/ec/edu/uce/dominio/MiFecha.java
+++ ProgramacionII/PAE_U2.1Fecha/src/main/java/ec/edu/uce/dominio/MiFecha.java
@@ -1,5 +1,9 @@
 package ec.edu.uce.dominio;
 
+/**
+ * @author Edwin Caiza
+ */
+
 public class MiFecha {
     private int dia;
     private int mes;
@@ -17,4 +21,86 @@ public class MiFecha {
         this.anio = anio;
     }
 
+    public int getDia() {
+        return dia;
+    }
+
+    public boolean setDia(int dia) {
+        if (dia > 0) {
+            boolean bisiesto = (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
+            switch (mes) {
+                case 1, 3, 5, 7, 8, 10, 12 -> {
+                    // Meses con 31 días
+                    if (dia <= 31){
+                        this.dia = dia;
+                        return true;
+                    }
+                }
+                case 4, 6, 9, 11 -> {
+                    // Meses con 30 días
+                    if (dia <= 30){
+                        this.dia = dia;
+                        return true;
+                    }
+                }
+                case 2 -> {
+                    if (bisiesto && dia <=29){
+                        this.dia = dia;
+                    }else if (dia <= 28){
+                        this.dia = dia;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public boolean setMes(int mes) {
+        if (mes > 0 && mes <= 12) {
+            this.mes = mes;
+            return true;
+        }
+        return false;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public boolean setAnio(int anio) {
+        if (anio > 0 && anio < 9999){
+            this.anio = anio;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean fechaValida(){
+        boolean bisiesto = (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
+
+        if (dia > 0 && (mes > 0 && mes <= 12) && (anio > 0 && anio <= 9999) ){
+            switch (mes) {
+                case 1, 3, 5, 7, 8, 10, 12 -> {
+                    // Meses con 31 días
+                    if (dia <= 31)
+                        return true;
+                }
+                case 4, 6, 9, 11 -> {
+                    // Meses con 30 días
+                    if (dia <= 30)
+                        return true;
+                }
+                case 2 -> {
+                    if (bisiesto && dia <= 29)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
 }
