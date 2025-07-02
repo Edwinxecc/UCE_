f6ec3084ba028ef40d97cea2a0ffe9988dd511dc
Edwin <caizadavid417@gmail.com>
2025-07-02 09:55:26 -0500

    Exception Class

---
+++ ProgramacionII/MiBanco/src/main/java/ec/edu/uce/util/DAOException.java
@@ -0,0 +1,15 @@
+package ec.edu.uce.util;
+
+public class DAOException extends Exception {
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException(Throwable cause) {
+        super(cause);
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ ProgramacionII/MiBanco/src/test/java/ec/edu/uce/dominio/Test.java
@@ -0,0 +1,9 @@
+package ec.edu.uce.dominio;
+
+public class Test {
+    public static void main(String[] args) {
+        //assert uso de argumentos -ea (habilitacion de asserts es para la aplicacion no para cualquier clase con main)
+        int x = 0;
+        assert (x==1);
+    }
+}
